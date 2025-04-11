be59f3e4451299e88d7a3fcecc30b3fb4beb7e75
John Collier <john.collier@mtabt.org>
2025-04-10 23:02:49 -0400

    Configure for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: true
+		})
 	},
 	preprocess: vitePreprocess()
 };
